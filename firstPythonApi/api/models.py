13be862a476fcfda33fe145e2040a89de7094f67
Miu <miu@Mius-MacBook-Pro.local>
2021-02-05 18:29:19 -0800

    changed database

--- firstPythonApi/api/models.py
+++ firstPythonApi/api/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
   name = models.CharField(max_length=50)
-  description = models.CharField(max_length=50)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  category = models.CharField(max_length=50)
+  brand = models.CharField(max_length=50)
+  description = models.CharField(max_length=500)
   image_url = models.CharField(max_length=200)
-
 
   def __str__(self):
       return self.name
