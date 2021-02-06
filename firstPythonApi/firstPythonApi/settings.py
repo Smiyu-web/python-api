d75aa9b8cab80a5de99f55ee3494c8a95ad29f7c
Miu <miu@Mius-MacBook-Pro.local>
2021-02-05 17:27:29 -0800

    fix: setting.py

--- firstPythonApi/firstPythonApi/settings.py
+++ firstPythonApi/firstPythonApi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'honp@$p4#pkh3#^%4b^g%ilv*(-)0opfxvp5%(8lq1i0837u5^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
